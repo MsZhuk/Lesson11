424c3006c038b4a1b072c7dc35f83a1bca760a38
Olya <ozhuk.fw@gmail.com>
2024-11-06 13:22:16 +0300

    src

--- src/pages/index.js
+++
@@ -1,6 +0,0 @@
-export * from './main.page';
-export * from './register.page';
-export * from './settings.page';
-export * from './app.page';
-export * from './article.page';
-export * from './burger.menu.page';
